a7e5defcd54ff76061c9a0fec9622d7f9cc4a501
treestae <treestae@gmail.com>
2020-01-10 23:58:37 +0900

    Inspection changed - RemoveCurlyBracesFromTemplate: Warning -> Information

--- src/Functions.kt
+++ src/Functions.kt
@@ -1,2 +1,23 @@
 package com.todoascoding.practice1
 
+fun printMessage(message: String): Unit {
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+    println("[${prefix}] ${message}")
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y
+
+fun main() {
+    printMessage("Hello")
+    printMessageWithPrefix("Hello", "Log")
+    printMessageWithPrefix("Hello")
+    printMessageWithPrefix(prefix = "Log", message = "Hello")
+    println(sum(1,2))
+}
